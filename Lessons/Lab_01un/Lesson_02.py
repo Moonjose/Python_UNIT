dedb309d3d25f2fcb01abfec625acd2b55e7dafb
Jose Antônio <ajose9975@gmail.com>
2024-08-08 00:29:19 -0300

    Lesson 02 improvements

--- Lessons/Lab_01un/Lesson_02.py
+++ Lessons/Lab_01un/Lesson_02.py
@@ -7,19 +7,40 @@ surprise1 = []
 surprise2 = []
 lottoresult = []
 correctNumbers = []
-numToAccept = 10
+numToAccept = 15
 
-while(True):    
+#---------------- Methods
+def testResults(listToIterate, resultName):
+    correctNumbers = []
+    for i in range(len(lottoresult)):  # 15 elements
+        for j in range(len(listToIterate)):  # 18 elements
+            if (lottoresult[i] == listToIterate[j]):
+                correctNumbers.append(listToIterate[j])
+    else:
+        if len(correctNumbers) >= numToAccept:
+            print('----------------------')
+            print(f'A lista {resultName} acertou o mínimo de {numToAccept} numeros! - Todos os numeros corretos: {correctNumbers}')
+
+def generateResults(startRange, endRange, listToAppend):
+    for i in range(startRange, endRange):
+        while True:
+            randToAdd = random.randint(1, 25)
+            if not (randToAdd in listToAppend):
+                listToAppend.append(randToAdd)
+                break
+
+#---------------- Main 
+while (True):
     print('--------------------------------------')
     dozensAmount = int((input('Digite a quantidade de dezenas que deseja marcar na primeira aposta: ')))
-    if (not(15 <= dozensAmount <= 18)):
+    if (not (15 <= dozensAmount <= 18)):
         print('--------------------------------------')
         print('Numero deve estar entre 15 e 18, digite novamente...')
     else:
         break
 
 for i in range(dozensAmount):
-    while(True):
+    while (True):
         print('--------------------------------------')
         numToAdd = int(input(f'Digite o {index + 1}° número: '))
 
@@ -27,72 +48,31 @@ for i in range(dozensAmount):
             print('--------------------------------------')
             print('Dezena deve estar entre 1 e 25, digite novamente...')
         elif numToAdd in dozens:
-           print('--------------------------------------')
-           print('Essa dezena já foi escolhida, escolha outra...')
+            print('--------------------------------------')
+            print('Essa dezena já foi escolhida, escolha outra...')
         else:
-             dozens.append(numToAdd)
-             index += 1
-             break
-
-
-for i in range(0, 18):
-    while True:
-        randToAdd = random.randint(1, 25)
-        if not (randToAdd in surprise1):
-            surprise1.append(randToAdd)
+            dozens.append(numToAdd)
+            index += 1
             break
 
-for i in range(0, 18):
-    while True:
-        randToAdd = random.randint(1, 25)
-        if not (randToAdd in surprise2):
-            surprise2.append(randToAdd)
-            break
+#---------------- Generate the lotto results and fill the lists
+generateResults(0, 18, surprise1)
+generateResults(0, 18, surprise2)
+generateResults(0, 15, lottoresult)
 
-for i in range(0, 15):
-    while True:
-        randToAdd = random.randint(1, 25)
-        if not (randToAdd in lottoresult):
-            lottoresult.append(randToAdd)
-            break
-    
+#---------------- Sort the lists
 dozens.sort()
 surprise1.sort()
 surprise2.sort()
 lottoresult.sort()
 
-print(f'Primeira dezena escolhida: {dozens}')
+#---------------- Show the lists
+print(f'Dezena escolhida: {dozens}')
 print(f'Primeira surpresinha: {surprise1}')
 print(f'Segunda surpresinha: {surprise2}')
 print(f'Resultado da Loto Facil: {lottoresult}')
 
-correctNumbers = []
-for i in range(len(lottoresult)): #15 elements
-    for j in range(len(surprise1)): # 18 elements
-        if(lottoresult[i] == surprise1[j]):
-            correctNumbers.append(surprise1[j])
-else:
-    if len(correctNumbers) >= numToAccept:
-        print('----------------------')
-        print(f'A surpresinha 1 acertou o mínimo de {numToAccept} numeros! - Todos os numeros corretos: {correctNumbers}')
-
-correctNumbers = []
-for i in range(len(lottoresult)): #15 elements
-    for j in range(len(surprise2)): # 18 elements
-        if(lottoresult[i] == surprise2[j]):
-            correctNumbers.append(surprise2[j])
-else:
-    if len(correctNumbers) >= numToAccept:
-        print('----------------------')
-        print(f'A surpresinha 2 acertou o mínimo de {numToAccept} numeros! - Todos os numeros corretos: {correctNumbers}')
-
-correctNumbers = []
-for i in range(len(lottoresult)): #15 elements
-    for j in range(len(dozens)): # 18 elements
-        if(lottoresult[i] == dozens[j]):
-            correctNumbers.append(dozens[j])
-else:
-    if len(correctNumbers) >= numToAccept:
-        print('----------------------')
-        print(f'A dezena escolhida acertou o mínimo de {numToAccept} numeros! - Todos os numeros corretos: {correctNumbers}')
-
+#---------------- Test the lists with lotto result
+testResults(surprise1, 'Surpresinha 1')
+testResults(surprise2, 'Surpresinha 2')
+testResults(dozens, 'Dezenas escolhidas')
